9f52f2377e5e75e85f38412d849081ab6c08ea51
Clara [Kimitayo] <84105466+Kimitayo@users.noreply.github.com>
2026-05-30 09:59:25 -0300

    adicionado normailizacao e distancia
    Added normalization and distance calculation for dataset.

--- kmeans.c
+++ kmeans.c
@@ -6,10 +6,12 @@
 #include <stdio.h>
 #include "tipos.h"
 #include "ingest.h"
+#include "normalize.h"
+#include "distance.h"
 
 //gcc kmeans.c ingest.c -o kmeans -lm
 
-#define K_CLUSTERS 3     // Quantidade de grupos que queremos encontrar
+#define K_CLUSTERS 3 // Quantidade de grupos que queremos encontrar
 #define MAX_ITER_ACOES 100 // Critério de parada por repetições
 
 int NUM_FEATURES = 0;
@@ -38,11 +40,21 @@ int main(int argc, char *argv[]) {
     const char *nomeArquivo = "vinhos.csv"; 
 
     Dataset *dataset = carregarDados(nomeArquivo);
+
+    // aplicar normalizaçao
+    normalizarDataset(dataset);
     Vinho *dados = dataset->dados;
     
     int tamanhoDataset = dataset->linhas;
-    alterarVariavelGlobal(dataset->colunas); // Atualiza a variável global com o número de colunas do dataset
+    alterarVariavelGlobal(dataset->colunas); // atualizar a variável global com o número de colunas do dataset
 
+    // aplicar distance.h 
+    double distancia = distanciaEuclidiana(dados[0].features, dados[1].features, NUM_FEATURES);
+
+    printf("\nDistancia entre vinho 0 e vinho 1: %f\n", distancia);
+
+
+    
     printf("Dados carregados: %d linhas e %d colunas (features) %d %d.\n", tamanhoDataset, NUM_FEATURES, dados[0].qualidade, dados[0].id);
 
     liberarDataset(dataset);
