99a0b735000f6d68148c03c25fdcf747c153ccc1
felipe130520 <felipe.lemos.barreto@gmail.com>
2026-06-09 15:08:24 -0300

    Paralelizacao da Normalização

--- Hybrid_MPI_OpenMP/normalize.c
+++ Hybrid_MPI_OpenMP/normalize.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<omp.h>
 #include "normalize.h"
 
 void normalizarDataset(Dataset *dataset) {
@@ -10,47 +11,41 @@ void normalizarDataset(Dataset *dataset) {
     double min[colunas];
     double max[colunas];
 
-    // inicializacao dos min e max
+    // Inicializacao dos min e max de forma paralela
+    #pragma omp parallel for schedule(static)
     for (int j = 0; j < colunas; j++) {
 
         min[j] = dataset->dados[0].features[j];
         max[j] = dataset->dados[0].features[j];
     }
 
-    // pra encontrar os min e max
+    // Para encontrar os min e max de forma paralela
+    #pragma omp parallel for schedule(static)
     for (int i = 0; i < linhas; i++) {
 
         for (int j = 0; j < colunas; j++) {
 
-            double valor =
-                dataset->dados[i].features[j];
+            double valor = dataset->dados[i].features[j];
 
-            if (valor < min[j]) {
-                min[j] = valor;
-            }
+            if (valor < min[j]) min[j] = valor;
+            if (valor > max[j]) max[j] = valor;
 
-            if (valor > max[j]) {
-                max[j] = valor;
-            }
         }
     }
 
-    // aplicar a nomralizacao
+    // Aplicando a normalizacao de forma paralela
+    #pragma omp parallel for collapse(2) schedule(static)
     for (int i = 0; i < linhas; i++) {
-
         for (int j = 0; j < colunas; j++) {
 
-            double valor =
-                dataset->dados[i].features[j];
-
+            double valor = dataset->dados[i].features[j];
+            double amplitude = max[j] - min[j];
             // Evita divisão por zero
-            if (max[j] - min[j] > 0) {
+            if (amplitude > 0) {
 
-                dataset->dados[i].features[j] =
-                    (valor - min[j]) /
-                    (max[j] - min[j]);
+                dataset->dados[i].features[j] = (valor - min[j]) / (amplitude);
             }else{
-                dataset->dados[i].features[j] = 0;
+                dataset->dados[i].features[j] = 0.0;
             }
         }
     }
